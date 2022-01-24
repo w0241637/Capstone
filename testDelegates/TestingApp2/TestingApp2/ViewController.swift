0bdbe141394706b50566e42b4b30624630752cad
Tim <52630462+mailmantim@users.noreply.github.com>
2022-01-23 21:04:42 -0400

    ui ideas and delegate test

---
+++ testDelegates/TestingApp2/TestingApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingApp2
+//
+//  Created by Tim Mailman on 2022-01-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
