d7aeccb5ff304b1a1d7f341d71732db3eae01650
Tim <52630462+mailmantim@users.noreply.github.com>
2022-02-09 17:29:49 -0400

    not writing to GraphQL DB on AWS

---
+++ main/TestingApp2/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testingapp2491f000a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "testingapp2": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
