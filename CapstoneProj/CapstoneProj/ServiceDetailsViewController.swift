b54379f8ffc8a59a53089df01dc03a9c3c2945a0
Tim <52630462+mailmantim@users.noreply.github.com>
2022-03-10 10:53:44 -0400

    map display, service request mutation

--- CapstoneProj/CapstoneProj/ServiceDetailsViewController.swift
+++ CapstoneProj/CapstoneProj/ServiceDetailsViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 import MapKit
 import CoreLocation
+import AWSMobileClient
+import AWSAppSync
+import Contacts
 
 class ServiceDetailsViewController: UIViewController {
 
@@ -21,24 +24,42 @@ class ServiceDetailsViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         if let user = appUser{
+            mapView.showsUserLocation = true
             let ann = MKPointAnnotation()
             ann.coordinate = CLLocationCoordinate2D(latitude: user.locLat, longitude: user.locLng)
             mapView.addAnnotation(ann)
             mapView.showAnnotations(mapView.annotations, animated: true)
         }
+        
+        lblName.text = appUser?.userName ?? "(No Name)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// create mutation to SvcReq table set service request
     @IBAction func doBtnServ(_ sender: Any) {
+        
+        guard let userLoc = ViewController.userLoc else {return}
+        
+        CLGeocoder().reverseGeocodeLocation(userLoc){(placemarks, error)in
+            guard error == nil else {return}
+            
+            guard let pm = placemarks?.first else {return}
+            
+            let addrFormatter = CNPostalAddressFormatter()
+            addrFormatter.style = .mailingAddress
+            guard let pa =  pm.postalAddress else {return}
+            let addr = addrFormatter.string(from:pa)
+            
+            let input = CreateSvcReqInput.init(custUName: AWSMobileClient.sharedInstance().username ?? "no cust name", provUName: self.appUser?.userName ?? "no prov name", svcLat: userLoc.coordinate.latitude, svcLng: userLoc.coordinate.longitude, svcAddr: addr)
+            let mut = CreateSvcReqMutation(input: input)
+            (UIApplication.shared.delegate as! AppDelegate).appSyncClient?.perform(mutation: mut, resultHandler:{
+                (result, error) in
+                print (error?.localizedDescription)
+                print (result)
+            })
+            
+            
+        }
+        
+//        let input = CreateSvcReqInput.init(custUName: AWSMobileClient.sharedInstance().username ?? "no cust name", provUName: appUser?.userName ?? "no prov name", svcLat: userLoc.coordinate.latitude, svcLng: userLoc.coordinate.longitude, svcAddr: addr)
     }
 }
