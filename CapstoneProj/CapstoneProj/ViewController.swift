31ddf63610e0ec69ab887fa0b55b572233d8b9fa
Tim <52630462+mailmantim@users.noreply.github.com>
2022-04-10 19:40:27 -0300

    adding some testing methods

--- CapstoneProj/CapstoneProj/ViewController.swift
+++ CapstoneProj/CapstoneProj/ViewController.swift
@@ -110,9 +110,7 @@ class ViewController: UITableViewController {
         
         //filter out current user
         let q = ListAppUsersQuery()
-//        let filterInput03 = ModelAppUserFilterInput(ne: AWSMobileClient.sharedInstance().username)
-//        let filterInput02 = ModelAppUserFilterInput(!=: AWSMobileClient.sharedInstance().username)
-//        let filterInput04 = ModelStringInput(!=: AWSMobileClient.sharedInstance().username)
+
         let filterInput = ModelStringInput(ne: AWSMobileClient.sharedInstance().username)
         let filter = ModelAppUserFilterInput(userName:filterInput)
         q.filter = filter
@@ -138,20 +136,7 @@ class ViewController: UITableViewController {
     }
     
     
-//    func fetchSvcReqs(){
-//        let q = ListSvcReqsQuery()
-//
-////
-////        let filterInput = ModelStringInput(ne: AWSMobileClient.sharedInstance().username)
-////
-////        let filter = ModelAppUserFilterInput(userName:filterInput)
-////        q.filter = filter
-////
-//        appSyncClient?.fetch(query: q, cachePolicy: .fetchIgnoringCacheData, resultHandler: {(results, error) in
-//            guard error == nil else {return}
-//            print (results?.data?.listSvcReqs?.items ?? "no users")
-//        })
-//    }
+
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? ServiceDetailsViewController {
